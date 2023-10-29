d357b950856e910bce9e66b3d0d6f4b4ffcd037b
이광현 <rhkd4129@naver.com>
2023-10-29 23:30:12 +0900

    a

--- project/1.sql
+++ project/1.sql
@@ -1,9 +1,54 @@
 
---- 팀 프로젝트에 있는 모든 작업에 대한 상태 확인 -> 도넛 차트로?  (groupby에  1,2를 지정하면 select에도 1,2 를 지정해야 한다
-select project_id ,count(*) from task
-group by task_status,project_id
-having project_id = 1;
 
--- 팀 프로젝트의 진척률 
-select  user_id ,task_status from task
-group by user_id,task_status;
+ -- TASK_STATUS ,
+select  count(*) as task_status_count from task
+where project_id = 1
+group by task_status
+order by TASK_STATUS;
+
+
+SELECT u.user_id, u.user_name, t.task_status, 
+       COALESCE(COUNT(t.task_status), 0) as status_count
+FROM USER_INFO u
+LEFT JOIN TASK t ON t.user_id = u.user_id and t.project_id = 1
+GROUP BY u.user_name, t.task_status, u.user_id
+ORDER BY u.user_name, t.task_status;
+
+-- 
+
+select t.user_id, u.user_name,t.task_status, count(t.task_status) status_count 
+from TASK t , USER_INFO u
+WHERE  t.project_id=1 and t.USER_ID = u.USER_ID
+group by u.user_name,t.task_status,t.user_id
+order by u.user_name, t.task_status;
+
+-- board
+select  project_id , task_status ,task_subject from task order by TASK_STATUS;
+
+
+--table
+select  rownum rn , a.*
+    from ( select u.user_name ,t.*  from task t , user_info u
+where t.user_id = u.user_id)
+
+
+
+SELECT * 
+from (SELECT rownum rn, a.*
+        FROM (select u.user_name ,t.*
+            FROM task t, user_info u
+            where t.user_id = u.user_id)a
+        )
+    where rn between 1 and 5;
+        
+			  		
+
+SELECT rownum rn, a.*
+        FROM (select u.user_name ,t.*
+            FROM task t, user_info u
+            where t.user_id = u.user_id)a
+    
+
+
+
+
